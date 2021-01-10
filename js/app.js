39e8393534b2e30e425292e0c8818750ae491764
Paul Bodenstab <bodenstabp@gmail.com>
2021-01-10 18:06:19 -0500

    chart.js functionality added

---
+++ js/app.js
@@ -0,0 +1,253 @@
+const trafficChart = document.getElementById('traffic-chart').getContext('2d')
+const dailyTrafficChart = document.getElementById('daily-traffic-chart').getContext('2d');
+const mobileUserChart = document.getElementById('mobile-user-chart').getContext('2d');
+const user = document.getElementById('userField');
+const message = document.getElementById('messageField');
+const send = document.getElementById('send');
+const trafficNav = document.getElementsByClassName('traffic-nav')
+const trafficNavLink = document.getElementsByClassName('traffic-nav-link');
+
+const hourlyData = {
+    labels: ['12-1','1-2','2-3','3-4','4-5','5-6','6-7','7-8','8-9','9-10','10-11','11-12', '12-1', '1-2','2-3','3-4','4-5','5-6','6-7','7-8','8-9','9-10','10-11','11-12',],
+    datasets: [{
+        label: '',
+        data: [10,5,5,0,5,10,10,20,30,25,15,30,20,20,30,35,30,45,35,20,30,25,30,15],
+        backgroundColor: 'rgba(116, 119, 191, 0.3)',
+        borderColor: 'rgba(123, 128, 190, 1)',
+        borderWidth: 2,
+        lineTension: 0,
+    }]
+}
+const hourlyOptions = {
+    aspectRatio: 2.5,
+    legend: {
+        display: false
+    },
+    scales: {
+        xAxes:[{
+            // offset: true,
+            gridLines:{
+            offsetGridLines: true,    
+        }
+        }],       
+        yAxes: [{
+            ticks: {
+                min: 0,
+                max: 100,
+            }
+        }]
+    }
+}
+const dailyData = {
+    labels: ['S', 'M', 'T', 'W', 'Th', 'F', 'Sa'],
+    datasets: [{
+        label: '',
+        data: [300, 400, 200, 225, 125, 600, 500],
+        backgroundColor: 'rgba(116, 119, 191, 0.3)',
+        borderColor: 'rgba(123, 128, 190, 1)',
+        borderWidth: 2,
+        lineTension: 0,
+    }]
+}
+const dailyOptions = {
+    aspectRatio: 2.5,
+    legend: {
+        display: false
+    },
+    scales: {
+        xAxes:[{
+            // offset: true,
+            gridLines:{
+            offsetGridLines: true,    
+        }
+        }],   
+        yAxes: [{
+            ticks: {
+                min: 0,
+                max: 750,
+            }
+        }]
+    }
+}
+const weeklyData = {
+    labels: ['16-22', '23-29', '30-5', '6-12', '13-19', '20-26', '27-3', '4-10', '11-17', '18-24', '25-31'],
+    datasets: [{
+        label: '',
+        data: [750, 1250, 1000, 1500, 2000, 1500, 1250, 1750, 2250, 1750, 2250],
+        backgroundColor: 'rgba(116, 119, 191, 0.3)',
+        borderColor: 'rgba(123, 128, 190, 1)',
+        borderWidth: 2,
+        lineTension: 0,
+    }]
+}
+const weeklyOptions = {
+    aspectRatio: 2.5,
+    legend: {
+        display: false
+    },
+    scales: {
+        xAxes:[{
+            // offset: true,
+            gridLines:{
+            offsetGridLines: true,    
+        }
+        }],
+        yAxes: [{
+            ticks: {
+                min: 0,
+                max: 2500,
+            }
+        }]
+    }
+}
+const monthlyData = {
+    labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+    datasets: [{
+        label: '',
+        data: [11000, 14000, 13500, 14050, 16000, 16200, 15500, 16550, 17000, 15900, 18000, 17250],
+        backgroundColor: 'rgba(116, 119, 191, 0.3)',
+        borderColor: 'rgba(123, 128, 190, 1)',
+        borderWidth: 2,
+        lineTension: 0,
+    }]
+}
+const monthlyOptions = {
+    aspectRatio: 2.5,
+    legend: {
+        display: false
+    },
+    scales: {
+        xAxes:[{
+            // offset: true,
+            gridLines:{
+            offsetGridLines: true,    
+        }
+        }],
+        
+        yAxes: [{
+            ticks: {
+                min: 0,
+                max: 20000,
+            }
+        }]
+    }
+}
+
+// Traffic Chart
+
+let initTrafficChart = new Chart(trafficChart, {
+    type: 'line',
+    data: weeklyData,
+    options: weeklyOptions
+});
+
+//Traffic Chart Controller
+
+trafficNav[0].addEventListener('click', event => {
+    if (!event.target.classList.contains('active')){
+        for(i = 0; i < 4; i++) {
+            if(trafficNavLink[i].classList.contains('active')){
+                trafficNavLink[i].classList.remove('active')
+            }
+        event.target.classList.add('active')
+        }
+    }
+    if (trafficNavLink[0].classList.contains('active')){
+        initTrafficChart = new Chart(trafficChart, {
+            type: 'line',
+            data: hourlyData,
+            options: hourlyOptions
+        });
+    } else if (trafficNavLink[1].classList.contains('active')) {
+        initTrafficChart = new Chart(trafficChart, {
+            type: 'line',
+            data: dailyData,
+            options: dailyOptions
+        });
+    } else if (trafficNavLink[2].classList.contains('active')) {
+        initTrafficChart = new Chart(trafficChart, {
+            type: 'line',
+            data: weeklyData,
+            options: weeklyOptions
+        });
+    } else if (trafficNavLink[3].classList.contains('active')) {
+        initTrafficChart = new Chart(trafficChart, {
+            type: 'line',
+            data: monthlyData,
+            options: monthlyOptions
+        });
+    }
+})
+
+
+
+
+// Daily Traffic Chart
+
+let initDailyTrafficChart = new Chart(dailyTrafficChart, {
+    type: 'bar',
+    data: {
+        labels: ['S', 'M', 'T', 'W', 'Th', 'F', 'Sa'],
+        datasets: [{
+            label: '',
+            data: [75, 100, 175, 125, 225, 200, 100],
+            backgroundColor: 'rgba(123, 128, 190, 1)',
+            borderColor: [],
+            borderWidth: [],
+        }]
+    },
+    options: {
+        aspectRatio: 2.5,
+        legend: {
+            display: false
+        },
+        scales: {
+            yAxes: [{
+                ticks: {
+                    min: 0,
+                    max: 250,
+                }
+            }]
+        }
+    }
+})
+
+// Mobile User Chart
+
+let initMobileUserChart = new Chart(mobileUserChart, {
+    type: 'doughnut',
+    data: {
+        labels: ['Desktops', 'Phones', 'Tablets'],
+        datasets: [{
+            label: '# of Users',
+            data: [2000, 550, 500],
+            backgroundColor: ['#7477BF', '#78CF82', '#51B6C8'],
+            borderColor: [],
+            borderWidth: 0,
+        }]
+    },
+    options: {
+        aspectRatio: 2.5,
+        legend: {
+            position: 'right',
+            labels: {
+                boxWidth: 20,
+                fontStyle: 'bold',
+            }
+        }
+    }
+})
+
+// Messdage User Controls
+send.addEventListener('click', () => {
+    if(user.value === '' && message.value === ''){
+        alert('Please fill out user and message fields before sending');
+    } else if (user.value === ''){
+        alert('Please fill out user field before sending.');
+    } else if (message.value === ''){
+        alert('Please fill out message field before sending.');
+    } else {
+        alert(`Message has been succesfully sent to ${user.value}`)
+    }
+})
+
